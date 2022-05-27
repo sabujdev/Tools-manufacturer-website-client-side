f4836c56d9860b61a5140e3a582e91686ea1e04f
sabujmahonto <sabujmahonto@gmail.com>
2022-05-28 00:26:40 +0600

    modify and banner section added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/blog" element={<Blog></Blog>}></Route>
-        Route>
+        
       </Routes>
     </div>
   );
